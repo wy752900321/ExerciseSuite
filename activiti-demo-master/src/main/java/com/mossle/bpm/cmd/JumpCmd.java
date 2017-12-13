0d35b503803f3287c48c0083ad5101fe8f176d0b
jiadongpo <295445156@qq.com>
2017-12-14 00:18:59 +0800

    工作流

---
+++ activiti-demo-master/src/main/java/com/mossle/bpm/cmd/JumpCmd.java
@@ -0,0 +1,41 @@
+package com.mossle.bpm.cmd;
+
+
+import org.activiti.engine.impl.interceptor.Command;
+import org.activiti.engine.impl.interceptor.CommandContext;
+import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
+import org.activiti.engine.impl.pvm.process.ActivityImpl;
+import org.activiti.engine.impl.pvm.process.ProcessDefinitionImpl;
+
+public class JumpCmd implements Command<Object> {
+	private String activityId;
+	private String processInstanceId;
+	private String jumpOrigin;
+
+	public JumpCmd(String processInstanceId, String activityId) {
+		this(processInstanceId,activityId,"jump");
+	}
+	public JumpCmd(String processInstanceId, String activityId , String jumpOrigin) {
+		this.activityId = activityId;
+		this.processInstanceId = processInstanceId;
+		this.jumpOrigin = jumpOrigin;
+	}
+
+	public Object execute(CommandContext commandContext) {
+        //parent execution
+		ExecutionEntity executionEntity = commandContext.getExecutionEntityManager().findExecutionById(processInstanceId);
+
+	        //1. 删除当前execution的所有子execution
+	        //2. 删除当前execution下的所有task
+	        //3. 删除当前execution下的所有job
+	        executionEntity.destroyScope(jumpOrigin);
+
+		ProcessDefinitionImpl processDefinition = executionEntity.getProcessDefinition();
+		ActivityImpl activity = processDefinition.findActivity(activityId);
+
+		executionEntity.executeActivity(activity);
+
+
+		return null;
+	}
+}
